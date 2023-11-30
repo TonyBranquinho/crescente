0195e9196ae5247676b104a4fad2a4080ad3ee53
Tony <antony.branquinho@gmail.com>
2023-11-30 12:26:03 -0300

    legal

---
+++ crescente/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crescente {
+    class Program {
+        static void Main(string[] args) {
+
+            int x, y;
+
+            Console.WriteLine("Digite dois numeros:");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x < y) {
+                Console.WriteLine("Crescente.");
+            }
+            else if (x > y) {
+                Console.WriteLine("Decrescente.");
+            }
+
+            while (x != y) {
+                Console.WriteLine("Digite dois numeros:");
+                x = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+
+                if (x < y) {
+                    Console.WriteLine("Crescente.");
+                }
+                else if (x > y) {
+                    Console.WriteLine("Decrescente.");
+                }
+            }
+        }
+    }
+}
